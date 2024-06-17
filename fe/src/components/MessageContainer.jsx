e6307e69673f4dc2e7f4bd10461996135ddbaf73
eluzung <luzungelaine@gmail.com>
2024-06-17 12:57:13 -0400

    fix: User messages appear on the right side of the screen.

--- fe/src/components/MessageContainer.jsx
+++ fe/src/components/MessageContainer.jsx
@@ -4,13 +4,14 @@ import SmartToyIcon from "@mui/icons-material/SmartToy";
 import PersonIcon from "@mui/icons-material/Person";
 
 function MessageContainer({ messageList }) {
+  console.log(messageList)
   return (
     <div className="chat-container">
       {messageList.map((message, index) => (
-        <div key={index} className={`msg-container ${message.author === "User" ? "message-right" : "message-left"}`}>
-          <div className={`message ${message.author === "User" ? "user-message" : "assistant-message"}`}>
-            <div className="author">
-              {message.author === "User" ? <PersonIcon /> : <SmartToyIcon />} {message.author}
+        <div key={index} className={`msg-container ${message.role === "user" ? "message-right" : "message-left"}`}>
+          <div className={`message ${message.role === "user" ? "user-message" : "assistant-message"}`}>
+            <div className="role">
+              {message.role === "user" ? <PersonIcon /> : <SmartToyIcon />} {message.role}
             </div>
             <div className="content">{message.content}</div>
           </div>
